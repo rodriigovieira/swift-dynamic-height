85dab6f9e51f1ac2f85b80d85dc253acf7e5a6e5
Rodrigo Vieira <rodriigovieira@gmail.com>
2022-03-31 11:28:29 -0300

    updating custom table view cell

--- FoxboxTest/CustomTableViewCell.swift
+++ FoxboxTest/CustomTableViewCell.swift
@@ -2,27 +2,55 @@
 //  CustomTableViewCell.swift
 //  FoxboxTest
 //
-//  Created by Rodrigo Vieira on 29/03/22.
+//  Created by Rodrigo Vieira on 31/03/22.
 //
 
 import UIKit
 
 class CustomTableViewCell: UITableViewCell {
+    let titleLabel = UILabel()
+    let overviewLabel = UILabel()
     
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var overviewLabel: UILabel!
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureLabels()
+    }
     
-    func setMovieValues(movie: MovieModel) {
-        titleLabel.text = movie.title
-        overviewLabel.text = movie.overview
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func setLabels(title: String, overview: String) {
+        titleLabel.text = "Title: \(title)"
+        overviewLabel.text = "Overview: \(overview)"
     }
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    func configureLabels() {
+        addSubview(titleLabel)
+        addSubview(overviewLabel)
+        
+        overviewLabel.numberOfLines = 0
+        
+        configureTitleConstraints()
+        configureOverviewConstraints()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    
+    func configureTitleConstraints() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 20).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 20).isActive = true
+        trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: 20).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20).isActive = true
     }
-
+    
+    func configureOverviewConstraints() {
+        overviewLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        overviewLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10).isActive = true
+        overviewLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20).isActive = true
+        trailingAnchor.constraint(equalTo: overviewLabel.trailingAnchor, constant: 20).isActive = true
+        overviewLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 20).isActive = true
+        overviewLabel.bottomAnchor.constraint(greaterThanOrEqualTo: bottomAnchor, constant: 20).isActive = true
+    }
 }
